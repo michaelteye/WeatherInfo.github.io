c49063e369ca72a09b53b584826a6801871c59a9
michaelteye <michaelteyegyemah@gmail.com>
2022-05-04 22:32:19 +0000

    Javascript Weather App

---
+++ try.js
@@ -0,0 +1,32 @@
+var inputVal = document.querySelector('#cityinput')
+var btn  = document.querySelector('#add');
+var city = document.querySelector('#cityoutput')
+var descrip = document.querySelector('#description')
+
+var temp = document.querySelector('#temp')
+var wind = document.querySelector('#wind') 
+
+apik =  "3045dd712ffe6e702e3245525ac7fa38"
+
+function convertion(val){
+    return (val - 273).toFixed(2)
+}
+// Now collect all the information with the help of fetch method
+btn.addEventListener('click', function(){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + inputVal.value+'&appid='+apik)
+    .then(res => res.json())
+    .then(data=> {
+        var nameval = data['name']
+        var descrip = data['weather']['0']['description']
+        var temperature = data['main']['temp']
+        var wndspd = data['wind']['speed']
+
+        city.innerHTML = `Weather of <span> ${nameval}</span>`
+        temp.innerHTML = `Temperature: <span>${ convertion(temperature)}C </span>`
+        descrip.innerHTML = `Sky Conditions: <span>${descrip}<span>`
+        wind.innerHTML = `Wind Speed: <span>${wndspd} km/h<span>`
+        
+    })
+    .catch(err => alert('You entered wrong city name'))
+
+})
